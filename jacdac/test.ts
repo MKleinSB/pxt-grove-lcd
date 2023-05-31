ff9f1f0a6de48b869bae349e56bd08bd5c55be71
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2023-05-31 23:38:16 +0200

    Aktualisiere jacdac/pxt.json, jacdac/test.ts

--- jacdac/test.ts
+++ jacdac/test.ts
@@ -1,9 +1,7 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
 input.onButtonPressed(Button.A, function() {
-    modules.characterScreen1.setLine(0, ":(")
+    modules.characterScreen1.setLine(0, "12345678901234567")
 })
-
-
 input.onButtonPressed(Button.B, function () {
-    modules.characterScreen1.setLine(1, ":)")
+modules.characterScreen1.setLineValue(1, "Light", 123)   
 })
